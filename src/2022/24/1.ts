1220c164aad66c6d8117086329605c4cd9128131
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-12-24 22:20:48 +0800

    :sparkles: 2022/24/1

--- src/2022/24/1.ts
+++ src/2022/24/1.ts
@@ -18,12 +18,18 @@ type Position = {
 
 type Blizzard = Position & { type: BlizzardType };
 
-const blizzards: Blizzard[] = [];
+let blizzards: Blizzard[] = [];
+
+const emptyMap: ('#' | '.')[][] = Array.from(Array(textMap.length), () => Array(textMap[0].length).fill('.'));
+
 
 for (const [y, line] of textMap.entries()) {
   for (const [x, tile] of line.entries()) {
     if (!['.', '#'].includes(tile)) {
       blizzards.push({ x, y, type: tile as BlizzardType });
+    }
+    if (tile === '#') {
+      emptyMap[y][x] = '#';
     }
   }
 }
@@ -61,86 +67,97 @@ const moveBlizzards = (blizzards: Blizzard[]): Blizzard[] => {
 const start = { x: 1, y: 0 };
 const end = { x: textMap[0].length - 2, y: textMap.length - 1 };
 
-type State = {
-  position: Position;
-  blizzards: Blizzard[];
-  minute: number;
-}
 
-const queue: State[] = [{ position: start, blizzards, minute: 0 }];
-let minMoves = Infinity;
-let i = 0;
-while (queue.length) {
-  i++;
-  // TODO: Stop if too far.
-  const { position, blizzards, minute }: State = JSON.parse(JSON.stringify(queue.shift()!));
-  if (!(i % 1000)) {
-    console.log('i', i, queue.length, minute);
-  }
-  if (position.x === end.x && position.y === end.y) {
-    console.log('Found a path!', minute);
-    minMoves = Math.min(minMoves, minute);
-    continue;
-  }
+const positionToString = ({ x, y }: Position): string => `${x},${y}`;
+const stringToPosition = (str: string): Position => {
+  const [x, y] = str.split(',').map(Number);
+  return { x, y };
+};
 
-  const newMinute = minute + 1;
+// const log = () => {
+//   const map: string[][] = emptyMap.map((line) => [...line]);
+//   for (const { x, y, type } of blizzards) {
+//     map[y][x] = type;
+//   }
+//   for (const { x, y } of positions) {
+//     map[y][x] = 'O';
+//   }
+//   console.log(map.map((line) => line.join('')).join('\n'));
+// }
+
+
+let positions: Position[] = [start];
+let minute = 1;
+while (true) {
+  // if (minute < 20) {
+
+  //   console.log('minute', minute);
+  //   log();
+  // }
+
   const newPositions: Position[] = [];
+  // TODO: Stop if too far.
+  // if (!(minute % 1000)) {
+  //   console.log('i', minute, positions.length, minute);
+  // }
   const newBlizzards = moveBlizzards(blizzards);
 
-  // Up:
-  let canMoveUp = false;
-  if (position.y > 1) {
-    canMoveUp = !newBlizzards.some((blizzard) => blizzard.x === position.x && blizzard.y === position.y - 1);
-  } else if (position.y === start.y + 1 && position.x === start.x) {
-    // We can move up only to go back to the start.
-    canMoveUp = true;
-  }
-  if (canMoveUp) {
-    newPositions.push({ x: position.x, y: position.y - 1 });
-  }
+  for (const position of positions) {
+    // Up:
+    let canMoveUp = false;
+    if (position.y > 1) {
+      canMoveUp = !newBlizzards.some((blizzard) => blizzard.x === position.x && blizzard.y === position.y - 1);
+    } else if (position.y === start.y + 1 && position.x === start.x) {
+      // We can move up only to go back to the start.
+      canMoveUp = true;
+    }
+    if (canMoveUp) {
+      newPositions.push({ x: position.x, y: position.y - 1 });
+    }
 
-  // Down:
-  let canMoveDown = false;
-  if (position.y < lengthY - 2) {
-    canMoveDown = !newBlizzards.some((blizzard) => blizzard.x === position.x && blizzard.y === position.y + 1);
-  } else if (position.y === end.y - 1 && position.x === end.x) {
-    // We can move down only to go to the end.
-    canMoveDown = true;
-  }
-  if (canMoveDown) {
-    newPositions.push({ x: position.x, y: position.y + 1 });
-  }
+    // Down:
+    let canMoveDown = false;
+    if (position.y < end.y - 1) {
+      canMoveDown = !newBlizzards.some((blizzard) => blizzard.x === position.x && blizzard.y === position.y + 1);
+    } else if (position.y === end.y - 1 && position.x === end.x) {
+      // We can move down only to go to the end.
+      canMoveDown = true;
+    }
+    if (canMoveDown) {
+      newPositions.push({ x: position.x, y: position.y + 1 });
+    }
 
-  // Left:
-  let canMoveLeft = false;
-  if (position.x > 1) {
-    canMoveLeft = !newBlizzards.some((blizzard) => blizzard.x === position.x - 1 && blizzard.y === position.y);
-  }
-  if (canMoveLeft) {
-    newPositions.push({ x: position.x - 1, y: position.y });
-  }
+    // Left:
+    let canMoveLeft = false;
+    if (position.x > 1 && position.y !== start.y && position.y !== end.y) {
+      canMoveLeft = !newBlizzards.some((blizzard) => blizzard.x === position.x - 1 && blizzard.y === position.y);
+    }
+    if (canMoveLeft) {
+      newPositions.push({ x: position.x - 1, y: position.y });
+    }
 
-  // Right:
-  let canMoveRight = false;
-  if (position.x < lengthX - 2) {
-    canMoveRight = !newBlizzards.some((blizzard) => blizzard.x === position.x + 1 && blizzard.y === position.y);
-  }
-  if (canMoveRight) {
-    newPositions.push({ x: position.x + 1, y: position.y });
-  }
+    // Right:
+    let canMoveRight = false;
+    if (position.x < end.x && position.y !== start.y && position.y !== end.y) {
+      canMoveRight = !newBlizzards.some((blizzard) => blizzard.x === position.x + 1 && blizzard.y === position.y);
+    }
+    if (canMoveRight) {
+      newPositions.push({ x: position.x + 1, y: position.y });
+    }
 
-  // No move:
-  if (!newBlizzards.some((blizzard) => blizzard.x === position.x && blizzard.y === position.y)) {
-    newPositions.push(position);
+    // No move:
+    if (!newBlizzards.some((blizzard) => blizzard.x === position.x && blizzard.y === position.y)) {
+      newPositions.push(position);
+    }
   }
-
-  queue.push(
-    ...newPositions.map((position) => {
-      return { position, blizzards: newBlizzards, minute: newMinute };
-    }),
-  );
+  if (newPositions.some((position) => position.x === end.x && position.y === end.y)) {
+    console.log('Found', minute);
+    break;
+  }
+  positions = newPositions.map(positionToString).filter((str, index, arr) => arr.indexOf(str) === index).map(stringToPosition);
+  blizzards = newBlizzards;
+  minute++;
 }
 
-console.log(minMoves);
 
 
