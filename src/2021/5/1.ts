e023deec8b0ff19fffa0ea6de4ee98e3cf915c14
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-01-21 22:45:51 +0800

    :sparkles: 2021/05/1

---
+++ src/2021/5/1.ts
@@ -0,0 +1,50 @@
+import { readFileSync } from 'fs';
+import * as path from 'path';
+
+const textInput = readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
+
+type Point = {
+  x: number;
+  y: number;
+}
+
+const input: [Point, Point][] = textInput.split('\n').filter(Boolean).map((line) => {
+  const [start, end] = line.split(' -> ').map((point) => {
+    const [x, y] = point.split(',').map((num) => parseInt(num));
+    return { x, y };
+  });
+  return [start, end];
+});
+
+const isVerticalLine = (point1: Point, point2: Point): boolean => {
+  return point1.x === point2.x;
+}
+
+const isHorizontalLine = (point1: Point, point2: Point): boolean => {
+  return point1.y === point2.y;
+}
+
+const map = new Map<string, number>();
+
+const pointToKey = (point: Point): string => {
+  return `${point.x},${point.y}`;
+}
+
+for (const [start, end] of input) {
+  if (isHorizontalLine(start, end)) {
+    for (let x = Math.min(start.x, end.x); x <= Math.max(start.x, end.x); x++) {
+      const key = pointToKey({ x, y: start.y });
+      map.set(key, (map.get(key) ?? 0) + 1);
+    }
+  } else if (isVerticalLine(start, end)) {
+    for (let y = Math.min(start.y, end.y); y <= Math.max(start.y, end.y); y++) {
+      const key = pointToKey({ x: start.x, y });
+      map.set(key, (map.get(key) ?? 0) + 1);
+    }
+  }
+}
+
+const result = [...map.values()].filter((value) => value > 1).length;
+
+// 1556 < x
+console.log(result); // 6841
