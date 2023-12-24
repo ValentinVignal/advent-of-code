73d4d60de69d8bdcbf53d51ff0c03d8df4784352
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-12-24 15:05:00 +0100

    :alembic: Test orthogonal

---
+++ src/2023/24/2-utils.ts
@@ -0,0 +1,49 @@
+export type XY = { x: number; y: number };
+
+export type XYZ = XY & { z: number };
+
+export const getInnerProduct = (a: XYZ, b: XYZ) => {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+};
+
+export const getCrossProduct = (a: XYZ, b: XYZ) => {
+  return {
+    x: a.y * b.z - a.z * b.y,
+    y: a.z * b.x - a.x * b.z,
+    z: a.x * b.y - a.y * b.x,
+  };
+};
+
+export const isVectorNull = (a: XYZ) => !a.x && !a.y && !a.z;
+
+export const addXYZ = (a: XYZ, b: XYZ) => ({
+  x: a.x + b.x,
+  y: a.y + b.y,
+  z: a.z + b.z,
+});
+
+export const multiplyXYZ = (time: number, velocity: XYZ) => ({
+  x: time * velocity.x,
+  y: time * velocity.y,
+  z: time * velocity.z,
+});
+
+/**
+ * The plane is defined by the equation:
+ *
+ * ```
+ * a * x + b * y + c * z + d = 0
+ * ```
+ */
+export type Plane = {
+  a: number;
+  b: number;
+  c: number;
+  d: number;
+};
+
+export const isPointInPlane = (plane: Plane, point: XYZ) => {
+  return (
+    plane.a * point.x + plane.b * point.y + plane.c * point.z + plane.d === 0
+  );
+};
