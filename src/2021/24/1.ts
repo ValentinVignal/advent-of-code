6165165d664dc4dff93b5fa4322fe6d641be7737
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-10-11 14:00:00 +0100

    :sparkles: 2021/24/1

--- src/2021/24/1.ts
+++ src/2021/24/1.ts
@@ -39,70 +39,74 @@ const instructions = textInput
     return instruction;
   });
 
-let biggestNumber = 1e14 - 1;
-
-const indexOfZero = (n: number): number => {
-  return n.toString().indexOf("0");
+type State = {
+  w: number;
+  x: number;
+  y: number;
+  z: number;
 };
 
-const runMonad = (n: number): boolean => {
-  const memory = new Map<Variable, number>();
-  for (const variable of variables) {
-    memory.set(variable, 0);
+const possibleDigits = [9, 8, 7, 6, 5, 4, 3, 2, 1] as const;
+
+let j = 0;
+
+const cache = new Set<string>();
+
+const toHash = (i: number, state: State): string =>
+  `${i},${state.w},${state.x},${state.y},${state.z}`;
+
+const runMonad = (i: number, n: number[], state: State): [boolean, number] => {
+  j++;
+  if (!(j % 100000000)) {
+    console.log("j", j, "i", i, "n", n, "state", state);
+  }
+  if (i === instructions.length) {
+    return [state.z === 0, parseInt(n.join(""))];
   }
 
-  const inputs = n
-    .toString()
-    .split("")
-    .map((s) => parseInt(s));
-
-  for (const instruction of instructions) {
-    if (instruction.operator === Operator.Inp) {
-      memory.set(instruction.first, inputs.shift()!);
-      continue;
+  const instruction = instructions[i];
+  if (instruction.operator === Operator.Inp) {
+    const hash = toHash(i, state);
+    if (cache.has(hash)) {
+      return [false, 0];
     }
-    const first = memory.get(instruction.first)!;
-    const second = instruction.second!;
-    const secondValue =
-      typeof second === "number" ? second : memory.get(second)!;
-    let result: number;
-    switch (instruction.operator) {
-      case Operator.Add:
-        result = first + secondValue;
-        break;
-      case Operator.Mul:
-        result = first * secondValue;
-        break;
-      case Operator.Eql:
-        result = Number(first === secondValue);
-        break;
-      case Operator.Mod:
-        result = first % secondValue;
-        break;
-      case Operator.Div:
-        result = Math.trunc(first / secondValue);
-        break;
+    for (const digit of possibleDigits) {
+      const newState = structuredClone(state);
+      newState[instruction.first] = digit;
+      const result = runMonad(i + 1, [...n, digit], newState);
+      if (result[0]) {
+        return result;
+      }
     }
-    memory.set(instruction.first, result!);
+    cache.add(hash);
+    return [false, 0];
   }
-  return memory.get("z")! === 0;
+
+  const first = state[instruction.first];
+  const second = instruction.second!;
+  const secondValue = typeof second === "number" ? second : state[second];
+  let result: number;
+  switch (instruction.operator) {
+    case Operator.Add:
+      result = first + secondValue;
+      break;
+    case Operator.Mul:
+      result = first * secondValue;
+      break;
+    case Operator.Eql:
+      result = Number(first === secondValue);
+      break;
+    case Operator.Mod:
+      result = first % secondValue;
+      break;
+    case Operator.Div:
+      result = Math.trunc(first / secondValue);
+      break;
+  }
+  state[instruction.first] = result!;
+  return runMonad(i + 1, n, state);
 };
 
-let i = 0;
-while (true) {
-  i++;
-  if (!(i % 1000000)) {
-    console.log(i, biggestNumber);
-  }
-  const index = indexOfZero(biggestNumber);
-  if (index !== -1) {
-    biggestNumber -= 10 ** (13 - index);
-    continue;
-  }
-  if (runMonad(biggestNumber)) {
-    break;
-  }
-  biggestNumber--;
-}
+const result = runMonad(0, [], { w: 0, x: 0, y: 0, z: 0 })[1];
 
-console.log(biggestNumber);
+console.log(result); // 91699394894995
