7d12c26a5f26a2af4689637a8840ea2b8047581b
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-12-05 14:23:53 +0800

    :sparkles: 2024/5/2

--- src/2024/5/2.ts
+++ src/2024/5/2.ts
@@ -21,7 +21,7 @@ const updates: Update[] = updatesText
   .map((line) => line.split(","));
 
 const orderCorrectly = (update: Update): Update | null => {
-  const subOrders = orders.filter((order) => {
+  let subOrders = orders.filter((order) => {
     return update.includes(order.before) && update.includes(order.after);
   });
 
@@ -38,65 +38,18 @@ const orderCorrectly = (update: Update): Update | null => {
 
   // The update is not correctly ordered
 
-  // These 2 should old all the pages that are in the update or else the answer
-  // wouldn't be unique.
-  const allBefores = subOrders.map((order) => order.before);
-  const allAfters = subOrders.map((order) => order.after);
+  const orderedUpdate: Update = [];
+  let toConsume = [...update];
+  while (toConsume.length) {
+    const onlyInBefore = toConsume.find((page) => {
+      return !subOrders.some((order) => order.after === page);
+    })!;
 
-  const beforeCounts = allBefores.reduce((acc, page) => {
-    return acc.set(page, acc.get(page) ?? 0 + 1);
-  }, new Map<string, number>());
-  const afterCounts = allAfters.reduce((acc, page) => {
-    return acc.set(page, acc.get(page) ?? 0 + 1);
-  }, new Map<string, number>());
+    orderedUpdate.push(onlyInBefore);
+    toConsume = toConsume.filter((page) => page !== onlyInBefore);
+    subOrders = subOrders.filter((order) => order.before !== onlyInBefore);
+  }
 
-  const allBeforeSorted = allBefores.filter(
-    (value, index, array) => array.indexOf(value) === index
-  );
-  allBeforeSorted.sort((a, b) => beforeCounts.get(a)! - beforeCounts.get(b)!);
-  const allAftersSorted = allAfters.filter(
-    (value, index, array) => array.indexOf(value) === index
-  );
-  allAftersSorted.sort((a, b) => afterCounts.get(b)! - afterCounts.get(a)!);
-
-  const dummyOrder = [...allBefores, ...allAfters]
-    .filter((value, index, array) => array.indexOf(value) === index)
-    .sort((a, b) => {
-      if (subOrders.find((order) => order.before === a && order.after === b)) {
-        return 1;
-      } else if (
-        subOrders.find((order) => order.before === b && order.after === a)
-      ) {
-        return -1;
-      }
-      return 0;
-    });
-
-  const order = (current: Update): Update | null => {
-    const subSubOrders = subOrders.filter((order) => {
-      return current.includes(order.before) && current.includes(order.after);
-    });
-    for (const order of subSubOrders) {
-      const beforeIndex = current.indexOf(order.before);
-      const afterIndex = current.indexOf(order.after);
-      if (beforeIndex > afterIndex) {
-        return null;
-      }
-    }
-    if (current.length === update.length) return current;
-    const potentialNewPages = dummyOrder.filter(
-      (page) => !current.includes(page)
-    );
-    for (const page of potentialNewPages) {
-      const newUpdate = order([...current, page]);
-      if (newUpdate) {
-        return newUpdate;
-      }
-    }
-    return null;
-  };
-
-  const orderedUpdate = order([])!;
   return orderedUpdate;
 };
 
