f228271270fc13712c7e3a81efbf9517d0d7e4a6
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-12-26 16:36:59 +0800

    :sparkles: 2024/22/2

--- src/2024/22/2.ts
+++ src/2024/22/2.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import * as path from "path";
 
-const example = true;
+const example = false;
 
 const textInput = readFileSync(
   path.join(__dirname, `input${example ? "-example-3" : ""}.txt`),
@@ -36,7 +36,7 @@ const changesToString = (changes: number[]): string => changes.join("");
 
 const changesScore = new Map<string, number>();
 
-const secrets: Secret[][] = inputs.map((input) => {
+inputs.forEach((input) => {
   const secret: Secret[] = [];
   let previousSecret = input;
   const seenChanges = new Set<string>();
@@ -57,36 +57,6 @@ const secrets: Secret[][] = inputs.map((input) => {
   return secret;
 });
 
-let max = 0;
-let bestSequence: number[] = [];
+const max = Math.max(...changesScore.values());
 
-for (let a = -9; a <= 9; a++) {
-  console.log("a", a);
-  for (let b = -9; b <= 9; b++) {
-    for (let c = -9; c <= 9; c++) {
-      for (let d = -9; d <= 9; d++) {
-        const score = secrets
-          .map((secret): number => {
-            for (let i = 4; i < secret.length; i++) {
-              if (
-                secret
-                  .slice(i - 3, i + 1)
-                  .every((s, j) => s.change === [a, b, c, d][j])
-              ) {
-                return secret[i].price;
-              }
-            }
-            return 0;
-          })
-          .reduce((acc, value) => acc + value, 0);
-        if (score > max) {
-          max = score;
-          bestSequence = [a, b, c, d];
-          console.log("max", max, bestSequence);
-        }
-      }
-    }
-  }
-}
-
-console.log(max);
+console.log(max); // 1405
