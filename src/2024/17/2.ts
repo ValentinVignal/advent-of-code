41416d45e59d5ab62a2379f6c74201a8ee9aba4a
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-12-29 17:51:36 +0800

    :alembic: manual from end

--- src/2024/17/2.ts
+++ src/2024/17/2.ts
@@ -49,51 +49,54 @@ const instructions: Instruction[] = programText
     };
   });
 
-const unsuccessfulStates = new Set<string>();
-
 const stateToString = ({ a, b, c }: Registers, outputLength: number) => {
   return `${a},${b},${c},${outputLength}`;
 };
 
-// Looking at the outputs, it seems the input must finish by (base8):
-// - 17
-// - 36017
-// - 0134036017, 0476236017, 537236017
-// - 537236017
-// - 510537236017 -> Dead end
-// - 10537236017
-// - 60510537236017, 64510537236017 -> Dead end
-// - 0134036017, 0476236017, 537236017 -> Dead end
-// - 36017
-// - 1236017 -> Dead end
-// - 36017
-// - 4036017, 6236017, 7236017 -> Dead end
-// - 0
-// - 17, 61
-// - 6017, 5661, 5761, 7761
-// - 05661
-// - 036017, 05661
-// - 505661, 4036017
-// - 4505661, 4036017
-// - 34505661, 34036017
-// - 134505661, 134036017
+// Looking at the outputs, it seems the input must start with by (base8):
+// - 5
+// - 53, 57
+// - 532, 572
+// - 5322, 5722, 5325
+// - 53223, 57223, 53257
+// - 532235, 572235, 532577 -> Dead end
+// - 53223, 57223, 53257
+// - 532235
+// - 5322350
+// - 53223501, 532235014
+// - 532235013, 532235016 -> Retry ?
+// - 5322350134, 5322350135, 5322350137
+// - 53223501340, 53223501342, 53223501352, 53223501372, 53223501345, 53223501355, 53223501375, 53223501346, 53223501356, 53223501347, 53223501357, 53223501377
+// - 532235013403, 532235013423, 532235013523, 532235013723, 532235013463, 532235013563, 532235013725, 532235013755, 532235013775
+// - 5322350134036, 5322350134236, 5322350135236, 5322350137236, 5322350134636, 5322350135636
+// - 53223501340360, 53223501342360, 53223501352360, 53223501372360, 53223501346360, 53223501356360, 53223501340361, 53223501340365, 53223501342365, 53223501352365, 53223501372365, 53223501346365, 53223501356365 -> Dead end
+//
+// - 5322350164, 5322350165
+// - 53223501640, 53223501642, 5322350165
+// - 532235016403, 532235016423, 532235016523
+// - 5322350164036, 5322350164236, 5322350165236
+// - 53223501640360, 53223501642360, 53223501652360
 
-const bases = [0o0];
+const bases = [0o53223501, 0o53223501, 0o53223501];
 
-const startInc = 0;
+const startInc = -1;
 let inc = startInc;
 const getRegisterA = (): number => {
   const base = bases[inc % bases.length];
   const newInc = Math.floor(inc / bases.length);
   if (base === 0) return newInc;
-  return base + newInc * 0o10 ** (Math.floor(Math.log(base) / Math.log(8)) + 1);
+  return (
+    base *
+      0o10 ** ((!newInc ? 0 : Math.floor(Math.log(newInc) / Math.log(8))) + 1) +
+    newInc
+  );
 };
 
 while (true) {
   inc++;
   const registerA = getRegisterA();
   console.log("registerA", registerA.toString(8));
-  if (inc > startInc + 8 ** 6) break;
+  if (inc > startInc + 8 ** 6 * bases.length) break;
 
   const registers = {
     ...initialRegisters,
@@ -168,24 +171,17 @@ while (true) {
 
   while (
     getInstruction() &&
-    programText.startsWith(outputs.join(",")) &&
     [registers.a, registers.b, registers.c].every(
       (r) => r >= 0 && Number.isInteger(r)
     )
   ) {
     if (!instructionIndex) {
-      if (unsuccessfulStates.has(stateToString(registers, outputs.length))) {
-        break;
-      }
       visitedStates.add(stateToString(registers, outputs.length));
     }
     runInstruction();
   }
 
   if (getInstruction() || programText !== outputs.join(",")) {
-    for (const visitedState of visitedStates) {
-      unsuccessfulStates.add(visitedState);
-    }
     console.log("out", outputs.join(","));
     continue;
   } else {
