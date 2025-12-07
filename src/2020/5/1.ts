acc67f54a3a3a18e6d6a502afe924649dad72521
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-12-07 13:18:00 +0800

    :sparkles: 2020/5/1

---
+++ src/2020/5/1.ts
@@ -0,0 +1,16 @@
+import { readFileSync } from "fs";
+import * as path from "path";
+
+const textInput = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+const ids = textInput.split("\n").map((line) => {
+  const row = line.slice(0, 8).replaceAll("F", "0").replaceAll("B", "1");
+  const col = line.slice(7).replaceAll("L", "0").replaceAll("R", "1");
+  const rowNum = parseInt(row, 2);
+  const colNum = parseInt(col, 2);
+  return rowNum * 8 + colNum;
+});
+
+const maxId = Math.max(...ids);
+
+console.log(maxId); // 878
