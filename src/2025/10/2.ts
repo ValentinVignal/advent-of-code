555d5fa0fea4b4b13a7867f124aa89ae7ce1aebc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-12-10 15:24:50 +0800

    Use while loop and queue

--- src/2025/10/2.ts
+++ src/2025/10/2.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import * as path from "path";
 
-const example = true;
+const example = false;
 const textInput = readFileSync(
   path.join(__dirname, `input${example ? "-example" : ""}.txt`),
   "utf-8"
@@ -14,11 +14,20 @@ type Machine = {
   buttons: Button[];
 };
 
+const scalarProduct = (a: number[], b: number[]): number => {
+  return a.reduce((sum, val, index) => sum + val * b[index], 0);
+};
+
 const input: Machine[] = textInput
   .trim()
   .split("\n")
   .map((line) => {
     const [a, b] = line.split(" {");
+    const joltage = b
+      .substring(0, b.length - 1)
+      .split(",")
+      .map(Number);
+
     const buttons = a
       .split("] ")[1]
       .split(" ")
@@ -27,71 +36,70 @@ const input: Machine[] = textInput
           .substring(1, block.length - 1)
           .split(",")
           .map(Number);
-      });
-    const joltage = b
-      .substring(0, b.length - 1)
-      .split(",")
-      .map(Number);
-
+      })
+      .sort((a, b) => scalarProduct(b, joltage) - scalarProduct(a, joltage));
     return { joltage, buttons };
-  });
+  })
+  .slice(12, 13);
 
-type MachineState = Machine & {
+type MachineState = {
   state: number[];
+  presses: number;
 };
 
-const isStateValid = (state: MachineState): boolean =>
-  state.state.every((joltage, index) => joltage <= state.joltage[index]);
+const isStateValid = (machine: Machine, state: MachineState): boolean =>
+  state.state.every((joltage, index) => joltage <= machine.joltage[index]);
 
-const isStateFinal = (state: MachineState): boolean =>
-  state.state.every((joltage, index) => joltage === state.joltage[index]);
-
-const states: MachineState[] = input.map((machine) => ({
-  ...machine,
-  state: Array(machine.joltage.length).fill(0),
-}));
+const isStateFinal = (machine: Machine, state: MachineState): boolean =>
+  state.state.every((joltage, index) => joltage === machine.joltage[index]);
 
 const stateToString = (state: MachineState): string => {
   return `${state.state.join(",")}`;
 };
 
-const findFewestPresses = (machine: MachineState): number => {
-  const cache = new Map<string, number>();
-  const findFewestPressesRecursive = (machineState: MachineState): number => {
-    const stateKey = stateToString(machineState);
-    if (cache.has(stateKey)) {
-      return cache.get(stateKey)!;
+const findFewestPresses = (machine: Machine): number => {
+  const queue = [
+    {
+      state: machine.joltage.map(() => 0),
+      presses: 0,
+    },
+  ];
+  const visited = new Set<string>();
+
+  while (queue.length) {
+    const current = queue.shift()!;
+
+    const stateKey = stateToString(current);
+    if (visited.has(stateKey)) {
+      continue;
     }
 
-    if (isStateFinal(machineState)) {
-      console.log("Final state reached:", stateKey);
-      cache.set(stateKey, 0);
-      return 0;
-    }
-    if (!isStateValid(machineState)) {
-      return Infinity;
+    visited.add(stateKey);
+
+    if (isStateFinal(machine, current)) {
+      return current.presses;
     }
 
-    const newStates: MachineState[] = machineState.buttons.map((button) => {
-      const newState = structuredClone(machineState);
+    const newStates: MachineState[] = machine.buttons.map((button) => {
+      const newState = structuredClone(current);
       for (const lightIndex of button) {
         newState.state[lightIndex]++;
       }
+      newState.presses++;
       return newState;
     });
 
-    const pressesCounts =
-      1 + Math.min(...newStates.map(findFewestPressesRecursive));
-
-    cache.set(stateKey, pressesCounts);
-    return pressesCounts;
-  };
-
-  return findFewestPressesRecursive(machine);
+    for (const newState of newStates) {
+      if (isStateValid(machine, newState)) {
+        queue.push(newState);
+      }
+    }
+  }
+  throw new Error("No solution found");
 };
 
-const results = states.map((machine, index) => {
-  console.log(`Processing machine ${index + 1}/${states.length}`);
+const results = input.map((machine, index) => {
+  console.log(`Processing machine ${index + 1}/${input.length}`);
   return findFewestPresses(machine);
 });
 
