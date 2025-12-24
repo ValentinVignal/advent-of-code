53608cc6fb92a5c66ebe508518994a294c0724a2
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-12-24 18:55:35 +0100

    :sparkles: 2025/12/1

--- src/2025/12/1.ts
+++ src/2025/12/1.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import * as path from "path";
 
-const example = true;
+const example = false;
 const textInput = readFileSync(
   path.join(__dirname, `input${example ? "-example" : ""}.txt`),
   "utf-8"
@@ -9,21 +9,7 @@ const textInput = readFileSync(
 
 const blocksTextInput = textInput.split("\n\n");
 
-type ShapeLine = [boolean, boolean, boolean];
-type Shape = [ShapeLine, ShapeLine, ShapeLine];
-
 const regionsTextInput = blocksTextInput[blocksTextInput.length - 1];
-const shapesTextInput = blocksTextInput.slice(0, -1);
-
-/** The shape of the presents from the input */
-const shapes: Shape[] = shapesTextInput.map((shapeText) => {
-  return shapeText
-    .split("\n")
-    .slice(1)
-    .map(
-      (line) => line.split("").map((char) => char === "#") as ShapeLine
-    ) as Shape;
-});
 
 type Region = {
   width: number;
@@ -38,156 +24,14 @@ const regions = regionsTextInput.split("\n").map((line) => {
   return { width, height, presents } as Region;
 });
 
-const shapeToString = (shape: Shape): string => {
-  return shape
-    .map((line) => line.map((cell) => (cell ? "#" : ".")).join(""))
-    .join("\n");
-};
+const shapeArea = 7;
 
-const rotateShape = (shape: Shape): Shape => {
-  return [
-    [shape[2][0], shape[1][0], shape[0][0]],
-    [shape[2][1], shape[1][1], shape[0][1]],
-    [shape[2][2], shape[1][2], shape[0][2]],
-  ];
-};
-
-const flipXShape = (shape: Shape): Shape => {
-  return [
-    [shape[0][2], shape[0][1], shape[0][0]],
-    [shape[1][2], shape[1][1], shape[1][0]],
-    [shape[2][2], shape[2][1], shape[2][0]],
-  ];
-};
-
-const flipYShape = (shape: Shape): Shape => {
-  return [
-    [shape[2][0], shape[2][1], shape[2][2]],
-    [shape[1][0], shape[1][1], shape[1][2]],
-    [shape[0][0], shape[0][1], shape[0][2]],
-  ];
-};
-
-/**
- * The shapes form the input. But it also contains all its possible rotations
- * and flips. It also removed duplicates.
- */
-const shapesWithRotations = shapes.map((shape) => {
-  const shapes: Shape[] = [shape];
-  let rotatedShape = shape;
-  for (let i = 0; i < 3; i++) {
-    rotatedShape = rotateShape(rotatedShape);
-    shapes.push(rotatedShape);
-    let flippedXShape = flipXShape(rotatedShape);
-    shapes.push(flippedXShape);
-    let flippedYShape = flipYShape(rotatedShape);
-    shapes.push(flippedYShape);
-    let flippedXYShape = flipYShape(flippedXShape);
-    shapes.push(flippedXYShape);
-  }
-  return shapes.filter((shape, index, array) => {
-    const shapeString = shapeToString(shape);
-    return array.findIndex((s) => shapeToString(s) === shapeString) === index;
-  });
+const filteredRegions = regions.filter((region) => {
+  const presentCount = region.presents.reduce((a, b) => a + b, 0);
+  const totalArea = region.width * region.height;
+  return presentCount * shapeArea <= totalArea;
 });
 
-type Position = { x: number; y: number };
+const result = filteredRegions.length;
 
-const positionToString = (position: Position): string =>
-  `${position.x},${position.y}`;
-
-const canRegionFitAllThePresents = (region: Region): boolean => {
-  /**
-   * When placing the n-th present ( {@link indexInList} ) in the region, this
-   * function returns the index in the input list of presents (
-   * {@link indexInInput} ) that corresponds to the n-th present in the region.
-   * It basically returns the index shape of the present.
-   */
-  const getPresentIndexInInputFromIndexInList = (
-    indexInList: number
-  ): number => {
-    let indexInInput = 0;
-    for (const presentIndexInInput of region.presents) {
-      if (presentIndexInInput < indexInList) {
-        indexInInput++;
-        indexInList -= presentIndexInInput;
-      } else {
-        return indexInInput;
-      }
-    }
-    return indexInInput;
-  };
-
-  type State = {
-    presentIndexInList: number;
-    currentRegion: string[];
-  };
-
-  const stateToString = (state: State): string => {
-    return `${state.presentIndexInList}|${state.currentRegion.join(";")}`;
-  };
-
-  const cache: string[] = [];
-
-  const canRegionFitAllThePresentsRecursive = ({
-    presentIndexInList,
-    currentRegion,
-  }: State): boolean => {
-    const stateId = stateToString({ presentIndexInList, currentRegion });
-    if (cache.includes(stateId)) {
-      return false;
-    }
-
-    const presentId = getPresentIndexInInputFromIndexInList(presentIndexInList);
-    const shapeVariants = shapesWithRotations[presentId];
-
-    for (const shape of shapeVariants) {
-      for (let y = 0; y <= region.height - 3; y++) {
-        for (let x = 0; x <= region.width - 3; x++) {
-          const occupiedPositions: Position[] = [];
-          for (let shapeY = 0; shapeY < 3; shapeY++) {
-            for (let shapeX = 0; shapeX < 3; shapeX++) {
-              if (shape[shapeY][shapeX]) {
-                occupiedPositions.push({ x: x + shapeX, y: y + shapeY });
-              }
-            }
-          }
-          const occupiedPositionsString =
-            occupiedPositions.map(positionToString);
-          const canPlaceShape = occupiedPositionsString.every(
-            (positionString) => {
-              return !currentRegion.includes(positionString);
-            }
-          );
-          if (!canPlaceShape) {
-            continue;
-          }
-
-          const newRegion = [
-            ...currentRegion,
-            ...occupiedPositionsString,
-          ].sort();
-          if (
-            canRegionFitAllThePresentsRecursive({
-              presentIndexInList: presentIndexInList + 1,
-              currentRegion: newRegion,
-            })
-          ) {
-            return true;
-          }
-        }
-      }
-    }
-    cache.push(stateId);
-    return false;
-  };
-
-  return canRegionFitAllThePresentsRecursive({
-    presentIndexInList: 0,
-    currentRegion: [],
-  });
-};
-
-const result = regions.filter(canRegionFitAllThePresents).length;
-
-console.log(result);
+console.log("Result:", result); //
