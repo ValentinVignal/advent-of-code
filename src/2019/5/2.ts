ba4424674d407dbf0764b1cf3ec1b985885ec857
ValentinVignal <valentin.vignal.dev@outlook.fr>
2026-04-03 18:17:41 +0200

    :sparkles: 2019/5/2

---
+++ src/2019/5/2.ts
@@ -0,0 +1,118 @@
+import { readFileSync } from "fs";
+import * as path from "path";
+
+const textInput = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+const intCode = textInput.split(",").filter(Boolean).map(Number);
+
+enum Mode {
+  Position = 0,
+  Immediate = 1,
+}
+
+enum Instruction {
+  Add = 1,
+  Multiply = 2,
+  Input = 3,
+  Output = 4,
+  JumpIfTrue = 5,
+  JumpIfFalse = 6,
+  LessThan,
+  Equals,
+  Halt = 99,
+}
+
+const id = 5;
+
+let i = 0;
+while (true) {
+  const opCode = intCode[i];
+  if (opCode === Instruction.Halt) {
+    break;
+  }
+
+  const instruction = (opCode % 10) as Instruction;
+
+  if (
+    ![
+      Instruction.Add,
+      Instruction.Multiply,
+      Instruction.Input,
+      Instruction.Output,
+      Instruction.JumpIfTrue,
+      Instruction.JumpIfFalse,
+      Instruction.LessThan,
+      Instruction.Equals,
+    ].includes(instruction)
+  ) {
+    throw Error(`Unknown opCode ${opCode}, index ${i}`);
+  }
+
+  if (
+    [
+      Instruction.Add,
+      Instruction.Multiply,
+      Instruction.LessThan,
+      Instruction.Equals,
+    ].includes(instruction)
+  ) {
+    const mode1 = (Math.floor(opCode / 100) % 10) as Mode;
+    const mode2 = (Math.floor(opCode / 1000) % 10) as Mode;
+    const i1 = intCode[i + 1];
+
+    const v1 = mode1 === Mode.Immediate ? i1 : intCode[i1];
+    const i2 = intCode[i + 2];
+    const v2 = mode2 === Mode.Immediate ? i2 : intCode[i2];
+    const iResult = intCode[i + 3];
+
+    let result: number;
+
+    if (instruction === Instruction.Add) {
+      result = v1 + v2;
+    } else if (instruction === Instruction.Multiply) {
+      result = v1 * v2;
+    } else if (instruction === Instruction.LessThan) {
+      result = v1 < v2 ? 1 : 0;
+    } else {
+      result = v1 === v2 ? 1 : 0;
+    }
+    intCode[iResult] = result;
+    i += 4;
+  }
+
+  if ([Instruction.Input, Instruction.Output].includes(instruction)) {
+    let address = intCode[i + 1];
+    if (instruction === Instruction.Input) {
+      intCode[address] = id;
+    } else {
+      const mode = (Math.floor(opCode / 100) % 10) as Mode;
+      let value = address;
+      if (mode === Mode.Position) {
+        value = intCode[value];
+      }
+      console.log(value);
+    }
+    i += 2;
+  }
+
+  if ([Instruction.JumpIfTrue, Instruction.JumpIfFalse].includes(instruction)) {
+    const mode1 = (Math.floor(opCode / 100) % 10) as Mode;
+    const mode2 = (Math.floor(opCode / 1000) % 10) as Mode;
+    const i1 = intCode[i + 1];
+
+    const v1 = mode1 === Mode.Immediate ? i1 : intCode[i1];
+    const i2 = intCode[i + 2];
+    const v2 = mode2 === Mode.Immediate ? i2 : intCode[i2];
+
+    if (
+      (instruction === Instruction.JumpIfTrue && !!v1) ||
+      (instruction === Instruction.JumpIfFalse && !v1)
+    ) {
+      i = v2;
+    } else {
+      i += 3;
+    }
+  }
+}
+
+// 9217546
