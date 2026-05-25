dd4f29cc1a237fe95512b88cbb2b156097661aac
ValentinVignal <valentin.vignal.dev@outlook.fr>
2026-05-25 17:58:38 +0800

    :sparkles: 2019/21/1

---
+++ src/2019/21/1.ts
@@ -0,0 +1,238 @@
+import { readFileSync } from "node:fs";
+import * as path from "node:path";
+
+const textInput = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+const intCode = new Map<bigint, bigint>(
+  [
+    ...textInput.split(",").filter(Boolean).map(Number).map(BigInt).entries(),
+  ].map(([index, value]) => [BigInt(index), value]),
+);
+
+enum Mode {
+  Position = 0,
+  Immediate = 1,
+  Relative = 2,
+}
+
+enum Instruction {
+  Add = 1,
+  Multiply = 2,
+  Input = 3,
+  Output = 4,
+  JumpIfTrue = 5,
+  JumpIfFalse = 6,
+  LessThan = 7,
+  Equals = 8,
+  AdjustRelativeBase = 9,
+  Halt = 99,
+}
+
+enum ReadType {
+  Read,
+  Write,
+}
+
+class Program {
+  constructor(
+    private intCode: Map<bigint, bigint>,
+    private onOutput: (output: bigint) => Promise<void>,
+    private onInput: () => Promise<bigint>,
+    private halt: (() => Promise<boolean>) | null = null,
+  ) {}
+
+  private index: bigint = 0n;
+
+  private _hasHalted: boolean = false;
+
+  private base = 0n;
+
+  private valueWithMode(
+    value: bigint,
+    mode: Mode,
+    readType: ReadType = ReadType.Read,
+  ): bigint {
+    switch (readType) {
+      case ReadType.Read:
+        switch (mode) {
+          case Mode.Immediate:
+            return value;
+          case Mode.Position:
+            return this.intCode.get(value) ?? 0n;
+          case Mode.Relative:
+            return this.intCode.get(this.base + value) ?? 0n;
+        }
+      case ReadType.Write:
+        switch (mode) {
+          case Mode.Immediate:
+            throw Error("Immediate mode not supported for write operations");
+          case Mode.Position:
+            return value;
+          case Mode.Relative:
+            return this.base + value;
+        }
+    }
+  }
+
+  get hasHalted(): boolean {
+    return this._hasHalted;
+  }
+
+  async run(): Promise<void> {
+    while (true) {
+      const shouldHalt = this.halt ? await this.halt() : false;
+      if (shouldHalt) {
+        this._hasHalted = true;
+        return Promise.resolve();
+      }
+      const opCode = this.intCode.get(BigInt(this.index))!;
+      if (opCode === BigInt(Instruction.Halt)) {
+        this._hasHalted = true;
+        return Promise.resolve();
+      }
+
+      const instruction = Number(opCode % 10n) as Instruction;
+
+      if (
+        ![
+          Instruction.Add,
+          Instruction.Multiply,
+          Instruction.Input,
+          Instruction.Output,
+          Instruction.JumpIfTrue,
+          Instruction.JumpIfFalse,
+          Instruction.LessThan,
+          Instruction.Equals,
+          Instruction.AdjustRelativeBase,
+        ].includes(instruction)
+      ) {
+        throw Error(`Unknown opCode ${opCode}, index ${this.index}`);
+      }
+
+      if (
+        [
+          Instruction.Add,
+          Instruction.Multiply,
+          Instruction.LessThan,
+          Instruction.Equals,
+        ].includes(instruction)
+      ) {
+        const mode1 = Number(
+          BigInt(Math.floor(Number(opCode / 100n))) % 10n,
+        ) as Mode;
+        const mode2 = Number(
+          BigInt(Math.floor(Number(opCode / 1000n))) % 10n,
+        ) as Mode;
+        const mode3 = Number(
+          BigInt(Math.floor(Number(opCode / 10000n))) % 10n,
+        ) as Mode;
+        const i1 = this.intCode.get(this.index + 1n)!;
+
+        const v1 = this.valueWithMode(i1, mode1);
+        const i2 = this.intCode.get(this.index + 2n)!;
+        const v2 = this.valueWithMode(i2, mode2);
+        const iResult = this.intCode.get(this.index + 3n)!;
+        const vResult = this.valueWithMode(iResult, mode3, ReadType.Write);
+
+        let result: bigint;
+
+        if (instruction === Instruction.Add) {
+          result = v1 + v2;
+        } else if (instruction === Instruction.Multiply) {
+          result = v1 * v2;
+        } else if (instruction === Instruction.LessThan) {
+          result = v1 < v2 ? 1n : 0n;
+        } else {
+          result = v1 === v2 ? 1n : 0n;
+        }
+        this.intCode.set(vResult, result);
+        this.index += 4n;
+      }
+
+      if ([Instruction.Input, Instruction.Output].includes(instruction)) {
+        let address = this.intCode.get(this.index + 1n)!;
+        if (instruction === Instruction.Input) {
+          const mode = Number(
+            BigInt(Math.floor(Number(opCode / 100n))) % 10n,
+          ) as Mode;
+          this.intCode.set(
+            this.valueWithMode(address, mode, ReadType.Write),
+            await this.onInput(),
+          );
+        } else {
+          const mode = Number(
+            BigInt(Math.floor(Number(opCode / 100n))) % 10n,
+          ) as Mode;
+          const value = this.valueWithMode(address, mode);
+          await this.onOutput(value);
+        }
+        this.index += 2n;
+      }
+
+      if (
+        [Instruction.JumpIfTrue, Instruction.JumpIfFalse].includes(instruction)
+      ) {
+        const mode1 = Number(
+          BigInt(Math.floor(Number(opCode / 100n))) % 10n,
+        ) as Mode;
+        const mode2 = Number(
+          BigInt(Math.floor(Number(opCode / 1000n))) % 10n,
+        ) as Mode;
+        const i1 = this.intCode.get(this.index + 1n)!;
+
+        const v1 = this.valueWithMode(i1, mode1);
+        const i2 = this.intCode.get(this.index + 2n)!;
+        const v2 = this.valueWithMode(i2, mode2);
+
+        if (
+          (instruction === Instruction.JumpIfTrue && !!v1) ||
+          (instruction === Instruction.JumpIfFalse && !v1)
+        ) {
+          this.index = v2;
+        } else {
+          this.index += 3n;
+        }
+      }
+
+      if (instruction === Instruction.AdjustRelativeBase) {
+        const mode = Number(
+          BigInt(Math.floor(Number(opCode / 100n))) % 10n,
+        ) as Mode;
+        const value = this.valueWithMode(
+          this.intCode.get(this.index + 1n)!,
+          mode,
+        );
+        this.base += value;
+        this.index += 2n;
+      }
+    }
+  }
+}
+
+const main = async (): Promise<void> => {
+  let result: bigint;
+
+  const inputs = ["NOT C J", "AND D J ", "NOT A T ", "OR T J", "WALK"].concat();
+
+  const inputForProgram = inputs.join("\n") + "\n";
+
+  let inputIndex = 0;
+
+  const program = new Program(
+    structuredClone(intCode),
+    async (output) => {
+      result = output;
+    },
+    async () => {
+      const char = inputForProgram[inputIndex];
+      inputIndex++;
+      return BigInt(char.charCodeAt(0));
+    },
+  );
+
+  await program.run();
+
+  console.log(result!); // 19347995
+};
+
+main();
